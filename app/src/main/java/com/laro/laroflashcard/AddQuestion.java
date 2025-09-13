82ac022b80e5a4a8664b65f264ababd4c2cdcf5c
ronaldlaboss <ronaldlaboss@gmail.com>
2025-09-13 17:08:21 -0400

    click listener to return to mainActivity is added to back button in second activity

--- app/src/main/java/com/laro/laroflashcard/AddQuestion.java
+++ app/src/main/java/com/laro/laroflashcard/AddQuestion.java
@@ -2,13 +2,30 @@ package com.laro.laroflashcard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class AddQuestion extends AppCompatActivity {
+
+    private ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_question);
+
+        backButton = findViewById(R.id.back_btn);
+
+        returnToMain();
+    }
+
+    //return to main activity
+    private void returnToMain()
+    {
+        backButton.setOnClickListener(v -> {
+            startActivity(new Intent(AddQuestion.this, MainActivity.class));
+            this.finish();
+        });
     }
 }
